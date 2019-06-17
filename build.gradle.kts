bc9859a469d4b6730de126c15b724d3f1fa5b4a0
Julien Ponge <julien.ponge@gmail.com>
2019-06-17 10:18:12 +0200

    Move to 0.5-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ allprojects {
   apply(plugin = "maven-publish")
   apply(plugin = "signing")
 
-  version = "0.4"
+  version = "0.5-SNAPSHOT"
   group = "io.reactiverse"
 
   extra["vertxVersion"] = "3.7.1"
