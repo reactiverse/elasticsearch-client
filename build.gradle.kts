af4f94ef54bde2a3bdb9d5d1da83efbe3c9e5e27
Julien Ponge <julien.ponge@gmail.com>
2019-09-30 22:00:26 +0200

    Next version

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ allprojects {
   apply(plugin = "maven-publish")
   apply(plugin = "signing")
 
-  version = "0.6.0ec7.3.2"
+  version = "0.7.0-SNAPSHOT"
   group = "io.reactiverse"
 
   extra["vertxVersion"] = "3.8.1"
