5e950a22e9e28a16c03c40fc497ee6f33b606009
Julien Ponge <julien.ponge@gmail.com>
2019-11-13 10:30:13 +0100

    Version 0.7.0ec7.4.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ allprojects {
   apply(plugin = "maven-publish")
   apply(plugin = "signing")
 
-  version = "0.7.0-SNAPSHOT"
+  version = "0.7.0ec7.4.2"
   group = "io.reactiverse"
 
   extra["vertxVersion"] = "3.8.3"
