65860f6414ae9c8cfcc6010d827735358bb4c715
Julien Ponge <jponge@redhat.com>
2020-01-13 12:01:59 +0100

    Version 0.7.0ec7.5.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ allprojects {
   apply(plugin = "maven-publish")
   apply(plugin = "signing")
 
-  version = "0.8.0-SNAPSHOT"
+  version = "0.7.0ec7.5.1"
   group = "io.reactiverse"
 
   extra["vertxVersion"] = "3.8.4"
