65cbe7ed320aa57dec13771006dc06597823ced3
Julien Ponge <jponge@redhat.com>
2020-10-13 15:58:47 +0200

    Version updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ plugins {
   `java-library`
   `maven-publish`
   signing
-  id("com.github.ben-manes.versions") version "0.29.0"
+  id("com.github.ben-manes.versions") version "0.33.0"
 }
 
 allprojects {
@@ -30,8 +30,8 @@ allprojects {
   version = "0.8.4-SNAPSHOT"
   group = "io.reactiverse"
 
-  extra["vertxVersion"] = "3.9.2"
-  extra["elasticClientVersion"] = "7.8.0"
+  extra["vertxVersion"] = "3.9.3"
+  extra["elasticClientVersion"] = "7.9.2"
   extra["isReleaseVersion"] = !version.toString().endsWith("SNAPSHOT")
 
   if (!project.hasProperty("ossrhUsername")) {
@@ -164,6 +164,6 @@ signing {
 }
 
 tasks.wrapper {
-  gradleVersion = "6.5.1"
+  gradleVersion = "6.6.1"
   distributionType = Wrapper.DistributionType.ALL
 }
--- integration-tests/build.gradle.kts
+++ integration-tests/build.gradle.kts
@@ -25,12 +25,12 @@ dependencies {
 
   testImplementation("io.vertx:vertx-junit5:${vertxVersion}")
 
-  testImplementation("org.assertj:assertj-core:3.14.0")
-  testImplementation("org.testcontainers:elasticsearch:1.14.1")
-  testImplementation("org.testcontainers:junit-jupiter:1.14.1")
+  testImplementation("org.assertj:assertj-core:3.17.2")
+  testImplementation("org.testcontainers:elasticsearch:1.15.0-rc2")
+  testImplementation("org.testcontainers:junit-jupiter:1.15.0-rc2")
 
-  testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.2")
-  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.2")
+  testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
 }
 
 tasks.test {
